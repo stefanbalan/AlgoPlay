5470e8e15353f91fdf9c7f47eecddddb5db31dc7
Stefan Balan <stefan.balan@gmail.com>
2024-04-13 17:17:49 +0300

    P044

---
+++ AlgoPlay.Test/LeetCode/P044WildcardMatchingTests.cs
@@ -0,0 +1,38 @@
+﻿using AlgoPlay.LeetCode;
+
+namespace AlgoPlay.Test.LeetCode;
+
+public class P044WildcardMatchingTests
+{
+    [Theory]
+
+    [InlineData("a", "a", true)]
+    [InlineData("aa", "a", false)]
+    [InlineData("aa", "*", true)]
+    [InlineData("cb", "?a", false)]
+    [InlineData("aabcbcaaa", "aa*bc*aaa", true)]
+    [InlineData("aabcbcaaa", "*bc*", true)]
+    [InlineData("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "cddd" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "bcbcaaa", "*cddd*aaaaa*bc*", true)]
+    public void Test(string s, string p, bool expected)
+    {
+        var solution = new P044WildcardMatching();
+        var result = solution.IsMatch(s, p);
+        
+        Assert.Equal(expected, result);
+    }
+}
---
+++ AlgoPlay/LeetCode/P044WildcardMatching.cs
@@ -0,0 +1,59 @@
+﻿namespace AlgoPlay.LeetCode;
+
+//Given an input string (s) and a pattern (p), implement wildcard pattern matching with support for '?' and '*' where:
+// 
+// '?' Matches any single character.
+// '*' Matches any sequence of characters (including the empty sequence).
+// The matching should cover the entire input string (not partial).
+// 
+//  
+// 
+// Example 1:
+// 
+// Input: s = "aa", p = "a"
+// Output: false
+// Explanation: "a" does not match the entire string "aa".
+// Example 2:
+// 
+// Input: s = "aa", p = "*"
+// Output: true
+// Explanation: '*' matches any sequence.
+// Example 3:
+// 
+// Input: s = "cb", p = "?a"
+// Output: false
+// Explanation: '?' matches 'c', but the second letter is 'a', which does not match 'b'.
+//  
+// 
+// Constraints:
+// 
+// 0 <= s.length, p.length <= 2000
+// s contains only lowercase English letters.
+// p contains only lowercase English letters, '?' or '*'.
+
+public class P044WildcardMatching
+{
+    public bool IsMatch(string s, string p)
+    {
+        return IsMatch(s, s.Length-1, p, p.Length-1);
+    }
+
+    public bool IsMatch(string s, int si, string p, int pi)
+    {
+        if (p[pi] == s[si] || p[pi] == '?')
+        {
+            if (si == 0 && pi == 0) return true;
+            if (si == 0 || pi == 0) return false;
+            return IsMatch(s, si - 1, p, pi - 1);
+        }
+
+        if (p[pi] == '*')
+        {
+            if (pi == 0) return true;
+            if (si == 0) return IsMatch(s, si, p, pi - 1);
+            return IsMatch(s, si - 1, p, pi) || IsMatch(s, si, p, pi - 1);
+        }
+
+        return false;
+    }
+}
