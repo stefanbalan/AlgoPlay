2bcb5aed87267f6b1f7dda5e1c3bd4c2c8bba218
Stefan Balan <stefan.balan@gmail.com>
2024-05-02 23:12:34 +0300

    P144 binary tree pre-order traversal (#2)
    * initial
    
    * less stack, more tests
    
    * all stack

---
+++ AlgoPlay/LeetCode/Trees/P144BinaryTreePreOrder.cs
@@ -0,0 +1,112 @@
+﻿namespace AlgoPlay.LeetCode.Trees;
+
+public class P144BinaryTreePreOrder
+{
+    //144. Binary Tree Preorder Traversal
+
+    //Given the root of a binary tree, return the preorder traversal of its nodes' values.
+    // 
+    // Example 1:
+    // 
+    // 
+    // Input: root = [1,null,2,3]
+    // Output: [1,2,3]
+    // Example 2:
+    // 
+    // Input: root = []
+    // Output: []
+    // Example 3:
+    // 
+    // Input: root = [1]
+    // Output: [1]
+    //  
+    // 
+    // Constraints:
+    // 
+    // The number of nodes in the tree is in the range [0, 100].
+    // -100 <= Node.val <= 100
+    //  
+    // 
+    // Follow up: Recursive solution is trivial, could you do it iteratively?
+
+    public IList<int> PreorderTraversal(TreeNode? root)
+    {
+        //var maxStackSize = 0;
+        var result = new List<int>(100);
+        if (root == null) return result;
+
+        var current = root;
+        var stack = new Stack<TreeNode>(100);
+        stack.Push(root);
+        do
+        {
+            while (current is not null)
+            {
+                result.Add(current.val);
+                stack.Push(current);
+                current = current.left;
+            }
+
+            //maxStackSize = Math.Max(maxStackSize, stack.Count);
+            current = stack.Pop();
+
+            current = current.right;
+        } while (stack.Count > 0);
+
+        return result;
+    }
+
+    public IList<int> PreorderTraversal1(TreeNode? root) // all stack!
+    {
+        //var maxStackSize = 0;
+        var result = new List<int>(100);
+        if (root == null) return result;
+
+        TreeNode current;
+        var stack = new Stack<TreeNode>(100);
+        stack.Push(root);
+        while (stack.Count > 0)
+        {
+            current = stack.Pop();
+            result.Add(current.val);
+            if(current.right != null) stack.Push(current.right);
+            if(current.left != null) stack.Push(current.left);
+            //maxStackSize = Math.Max(maxStackSize, stack.Count);
+        }
+
+        return result;
+    }
+    
+    public IList<int> PreorderTraversal2(TreeNode? root) // I thought this would be faster but it's not
+    {
+        //var maxStackSize = 0;
+
+        var result = new List<int>(100);
+        if (root == null) return result;
+
+        var current = root;
+        var stack = new Stack<TreeNode>(100);
+
+        while (current is not null)
+        {
+            result.Add(current.val);
+            if (current.left is not null)
+            {
+                if (current.right != null) stack.Push(current.right);
+                current = current.left;
+            }
+            else if (current.right != null) current = current.right;
+
+            else if (stack.Count > 0)
+            {
+                //maxStackSize = Math.Max(maxStackSize, stack.Count);
+
+                current = stack.Pop();
+            }
+            else
+                current = null;
+        }
+
+        return result;
+    }
+}
---
+++ AlgoPlay/LeetCode/Trees/TreeNode.cs
@@ -0,0 +1,52 @@
+﻿namespace AlgoPlay.LeetCode.Trees;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+{
+    public int val = val;
+    public TreeNode? left = left;
+    public TreeNode? right = right;
+
+    public static TreeNode? FromArray(int?[] array)
+    {
+        if (array.Length == 0 || array[0] is null) return null;
+
+        var root = new TreeNode(array[0]!.Value);
+        var q = new Queue<TreeNode>(array.Length / 2 + 1); // the maximum number of nodes at the last level (leave nodes) 
+        q.Enqueue(root);
+
+        var i = 1;
+        while (i < array.Length && q.TryDequeue(out var node))
+        {
+            if (array[i] is not null)
+            {
+                node.left = new TreeNode(array[i]!.Value);
+                q.Enqueue(node.left);
+            }
+
+            i += 1;
+
+            if (i < array.Length && array[i] is not null)
+            {
+                node.right = new TreeNode(array[i]!.Value);
+                q.Enqueue(node.right);
+            }
+
+            i += 1;
+        }
+
+        return root;
+    }
+}
