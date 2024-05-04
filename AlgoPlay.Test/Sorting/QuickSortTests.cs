90b3610e87ca3771adcafddc4054fd07bbe51382
Stefan Balan <stefan.balan@gmail.com>
2024-05-04 19:11:18 +0300

    quicksort (#3)

---
+++ AlgoPlay.Test/Sorting/QuickSortTests.cs
@@ -0,0 +1,36 @@
+﻿using AlgoPlay.Sorting;
+
+namespace AlgoPlay.Test.Sorting
+{
+    public class QuickSortTests
+    {
+        [Theory, MemberData(nameof(TestData))]
+        public void IterativeTest(int[] arr, int[] exp)
+        {
+            QuickSort.Iterative(arr);
+            Assert.Equal(exp, arr);
+        }
+
+        [Theory, MemberData(nameof(TestData))]
+        public void RecursiveTest(int[] arr, int[] exp)
+        {
+            QuickSort.Recursive(arr);
+            Assert.Equal(exp, arr);
+        }
+
+
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return [new int[] { }, new int[] { }];
+            yield return [new int[] { 1 }, new int[] { 1 }];
+            yield return [new int[] { 1, 1 }, new int[] { 1, 1 }];
+            yield return [new int[] { 1, 2 }, new int[] { 1, 2 }];
+            yield return [new int[] { 2, 1 }, new int[] { 1, 2 }];
+            yield return
+            [
+                new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 },
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+            ];
+        }
+    }
+}
---
+++ AlgoPlay/Sorting/QuickSort.cs
@@ -0,0 +1,81 @@
+﻿namespace AlgoPlay.Sorting;
+
+public class QuickSort
+{
+    public static void Iterative(int[] arr)
+    {
+        if (arr.Length == 0) return;
+
+        var stack = new Stack<(int, int)>(arr.Length / 2 + 1);
+
+        stack.Push((0, arr.Length - 1));
+
+        while (stack.Count > 0)
+        {
+            var (low, high) = stack.Pop();
+
+            int pi = Partition(arr, low, high);
+
+            if (pi - 1 > low) stack.Push((low, pi - 1));
+            if (pi + 1 < high) stack.Push((pi + 1, high));
+        }
+    }
+
+
+    public static void Recursive(int[] arr)
+    {
+        Sort(arr, 0, arr.Length - 1);
+    }
+
+    private static void Sort(int[] arr, int low, int high)
+    {
+        if (low < high)
+        {
+            int pi = Partition(arr, low, high);
+
+            Sort(arr, low, pi - 1);
+            Sort(arr, pi + 1, high);
+        }
+    }
+
+    private static int PartitionMedian(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                Swap(arr, i, j);
+            }
+        }
+
+        Swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static int Partition(int[] arr, int low, int high)
+    {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++)
+        {
+            if (arr[j] < pivot)
+            {
+                i++;
+                Swap(arr, i, j);
+            }
+        }
+
+        Swap(arr, i + 1, high);
+        return i + 1;
+    }
+
+    private static void Swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
